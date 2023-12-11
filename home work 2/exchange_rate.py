f9ebcd0e184cdb5aa684e761e558e8a6ddff137b
SeleniaIrene <selenia.irish@ukr.net>
2023-12-11 18:51:12 +0200

    1. Серед методів строк знайти в документації той метод, який додасть декілька символів зліва від початку строки. 2.Подивитись на скріншот №1 та розібратись в чому суть помилки 3. Розширити функціонал обмінника. Додати можливість обирати операцію(купівля продаж). Для простоти лише в парі долар-гривня 4. Написати програму, яка розраховує індекс маси тіла якщо користувач може ввести значення ваги і зросту у різних системах виміру(по 2 на кожну)

--- home work 2/exchange_rate.py
+++ home work 2/exchange_rate.py
@@ -1,5 +1,6 @@
 # Задача Обміник:
-# Намалювати символами табло обміника. Додати 3 валюти. Курс продажу має розраховуватись як на 5% більший ніж покупка.
+# Намалювати символами табло обміника. Додати 3 валюти.
+# Курс продажу має розраховуватись як на 5% більший ніж покупка.
 # Також відформувати вивод обмінику. Щоб сумми отриманих грошей і решти були на одному рівні.
 # Шаблон табло
 # **************
@@ -35,4 +36,55 @@ print(f"* {buy_uah_pln:<11} PLN {sell_uah_pln:>11} *")
 print(f"* {buy_uah_dkk:<11} DKK {sell_uah_dkk:>11} *")
 print(f"* {buy_uah_gbp:<11} GBP {sell_uah_gbp:>11} *")
 print("*"*31)
+
+# Розширити функціонал обмінника. Додати можливість обирати операцію(купівля продаж).
+# Для простоти лише в парі долар-гривня 50 балів
+# При купівлі долару, якщо сумма в долларах близька до суми кратній 50 запропонувати взяти рівну суму.
+# Або повернути зайві гривні або попросити доплатити.
+# Та розрахувати нову сдачу в гривні, з урахуванням нової суми. Критерій близкості 5%.
+# Зауважте, що в майбутньому критерій близкості та кратність може змітинись.
+# Ці параметри мають зберігатись в константах, щоб ми мали змогу швидко їх змінити при необхідності.
+
+operation = input("Choose an operation (purchase - 'buy', sale - 'sell'): ").lower()
+
+if operation == 'buy':
+    print(f"{buy_uah_usd} USD")
+elif operation == 'sell':
+    print(f"{sell_uah_usd} USD")
+else:
+    print("You have entered an incorrect operation. Enter buy or sell")
+
+# Константи для критеріїв близькості та кратності
+CLOSENESS_THRESHOLD = 0.05
+MULTIPLE = 50
+change_uah = amount_uah % (buy_uah_usd * UAH_USD)
+
+if operation == 'buy':
+    print(f"Вы получите {buy_uah_usd} USD, сдача {change_uah:.0f} UAH.")
+
+    if buy_uah_usd % MULTIPLE != 0:
+        nearest_multiple = round(buy_uah_usd / MULTIPLE) * MULTIPLE
+        difference = nearest_multiple - buy_uah_usd
+
+        closeness = abs(difference / nearest_multiple)
+        if closeness <= CLOSENESS_THRESHOLD:
+            extra_to_pay = difference * UAH_USD
+            print(f"Чтобы купить {nearest_multiple}$, внесите {extra_to_pay:.2f} гривен.")
+            decision = input("Хотите внести недостающую сумму? (Y/n): ").lower()
+
+            if decision == 'y':
+                extra_payment = float(input("Введите сумму доплаты в гривнах: "))
+                amount_uah += extra_payment
+                new_buy_uah_usd = amount_uah // UAH_USD
+
+                if new_buy_uah_usd % MULTIPLE == 0:
+                    print(f"Вы получите {new_buy_uah_usd} USD, сдача {amount_uah % (new_buy_uah_usd * UAH_USD):.0f} UAH.")
+                else:
+                    new_change_uah = amount_uah % (new_buy_uah_usd * UAH_USD)
+                    print(f"Вы получите {new_buy_uah_usd} USD, сдача {new_change_uah:.0f} UAH.")
+            else:
+                print(f"Вы получите {buy_uah_usd} USD, сдача {change_uah:.0f} UAH.")
+        else:
+            extra_to_pay = difference * UAH_USD
+            print(f"Вам необходимо внести дополнительные гривны, чтобы купить {nearest_multiple}$. Внесите {extra_to_pay:.2f} гривен.")
 
